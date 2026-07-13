fa7ec577201c3dd96662c79c6418598650a2bece
Sc0rpX <mahfuzurrahman5609@gmail.com>
2026-07-14 00:47:54 +0600

    Remove iconify icon from the project and use Lucide iconset

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import { Download } from "lucide-react";
+
 function Header() {
     return (
         <nav className="px-6 py-4 flex justify-between bg-gray-100 shadow-md shrink-0">
             <h1 className="font-heading font-bold text-2xl text-blue-800">ResumeBuilder</h1>
             <button className="px-6 py-2 text-sm text-white font-medium bg-blue-800 rounded-sm flex items-center gap-2">
-                <span className="icon-[mdi--download] text-xl"></span>
+                <Download size={18}/>
                 Export to PDF
             </button>
         </nav>
