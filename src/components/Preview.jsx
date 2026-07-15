9a4b31ac4b8b5277cdf208451e2bd1c18e5e9ee1
Sc0rpX <mahfuzurrahman5609@gmail.com>
2026-07-16 04:04:06 +0600

    Refactor Preview component to include personal info and sections for professional summary, skills, education, and experience

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,8 +1,106 @@
-function Preview() {
-    return (
-        <div className="bg-gray-200 flex-1">
+import { Mail, Phone, Globe } from 'lucide-react';
 
-        </div>
+function Section({ name, children }) {
+    return (
+        <section>
+            <h3 className='text-lg font-bold text-blue-800 uppercase tracking-wide border-b-2 border-blue-800 mb-4'>
+                {name}
+            </h3>
+            {children}
+        </section>
+    )
+}
+
+function Preview({ personalInfo, professionalSummary, skills, education, experience }) {
+    return (
+        <section className="flex-1 bg-gray-200 overflow-y-auto p-10 flex justify-center">
+
+            {/*--- A4 Paper ---*/}
+            <div className="bg-white w-full max-w-[794px] min-h-[1123px] shadow-2xl p-16 border border-gray-200 flex flex-col gap-8">
+
+                {/*--- Header Section ---*/}
+                <section className="flex flex-col items-center mb-4">
+                    <h2 className="text-center text-3xl font-semibold uppercase tracking-wide">
+                        {personalInfo.fullName || "Your Full Name"}
+                    </h2>
+                    <span className="text-lg font-medium mb-3">{personalInfo.jobTitle || "Your Job Title"}</span>
+                    <div className="flex justify-center gap-x-4 flex-wrap">
+                        <span className='flex items-center gap-2'>
+                            <Mail size={20} />
+                            <span>{personalInfo.email || "demomail@example.com"}</span>
+                        </span>
+                        <span className='flex items-center gap-2'>
+                            <Phone size={20} />
+                            <span>{personalInfo.phone || "+880 19727-27460"}</span>
+                        </span>
+                        <span className='flex items-center gap-2'>
+                            <Globe size={20} />
+                            <span>{personalInfo.website || "your_username.com"}</span>
+                        </span>
+                    </div>
+                </section>
+
+                {/* --- Professional summury section --- */}
+                <Section name={"professional summary"}>
+                    <p className='text-base/relaxed'>{professionalSummary}</p>
+                </Section>
+
+                {/* --- Skills Section --- */}
+                <Section name={"skills"}>
+                    <div className='flex gap-2 flex-wrap'>
+                        {
+                            skills.map((title, index) => {
+                                return (
+                                    <span key={index} className='px-4 py-1.5 bg-gray-300'>
+                                        {title}
+                                    </span>
+                                )
+                            })
+                        }
+                    </div>
+                </Section>
+
+                {/* Education Section */}
+                <Section name={"Education"}>
+                        <div className='flex flex-col gap-5'>
+                            {
+                                education.map((item, index) => {
+                                    return (
+                                        <div>
+                                            <div className='flex justify-between items-center'>
+                                                <span className='text-xl font-bold'>{item.degree}</span>
+                                                <span>{`${item.startYear} - ${item.endYear}`}</span>
+                                            </div>
+                                            <div className='text-lg text-gray-700'>{item.institute}</div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                </Section>
+
+                {/* --- Experience ---  */}
+                <Section name={"Experience"}>
+                            <div className='flex flex-col gap-5'>
+                            {
+                                experience.map((item, index) => {
+                                    return (
+                                        <div>
+                                            <div className='flex justify-between items-center'>
+                                                <span className='text-xl font-bold'>{item.position}</span>
+                                                <span>{`${item.startYear} - ${item.endYear}`}</span>
+                                            </div>
+                                            <div className='text-lg text-blue-800 font-medium'>{item.company}</div>
+                                            <div className='pl-4 mt-2'>{item.description}</div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                </Section>
+            </div>
+
+        </section>
     )
 }
 
