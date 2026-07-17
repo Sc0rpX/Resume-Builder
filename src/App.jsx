e8e3dafb746c662e4ee448b9de242bc612882e5c
Sc0rpX <mahfuzurrahman5609@gmail.com>
2026-07-17 16:12:41 +0600

    Refactor App component for improved readability and structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header.jsx'
 import Preview from './components/Preview.jsx'
 
 function App() {
-  const [ personalInfo, setPersonalInfo ] = useState({
+  const [personalInfo, setPersonalInfo] = useState({
     fullName: "",
     jobTitle: "",
     email: "",
@@ -13,11 +13,11 @@ function App() {
     website: "",
   })
 
-  const [ professionalSummary, setProfessionalSummary] = useState("")
+  const [professionalSummary, setProfessionalSummary] = useState("Summarize your career highlights, key expertise, and professional goals here in 5-6 brief sentences. Focus on how you add value.")
 
-  const [ skills, setSkills ] = useState(["HTML", "CSS", "JAVASCRIPT"])
+  const [skills, setSkills] = useState(["HTML", "CSS", "JAVASCRIPT"])
 
-  const [ education, setEducation ] = useState([
+  const [education, setEducation] = useState([
     {
       degree: "BSc in CSE",
       institute: "BRAC University",
@@ -26,10 +26,10 @@ function App() {
     }
   ])
 
-  const [ experience, setexperience ] = useState([
+  const [experience, setExperience] = useState([
     {
       position: "Front-end Developer",
-      company: "Unisoft Ltd", 
+      company: "Unisoft Ltd",
       description: "Developed and maintained user-facing features using modern React and Vue frameworks.",
       startYear: "2024",
       endYear: "2026",
@@ -40,8 +40,26 @@ function App() {
     <div className='flex flex-col overflow-hidden w-screen h-screen'>
       <Header />
       <div className='flex flex-1 overflow-hidden'>
-        <Editor />
-        <Preview personalInfo={personalInfo} professionalSummary={professionalSummary} skills={skills} education={education} experience={experience} />
+        <Editor 
+          personalInfo={personalInfo} 
+          setPersonalInfo={setPersonalInfo} 
+          professionalSummary={professionalSummary} 
+          setProfessionalSummary={setProfessionalSummary}
+          skills={skills} 
+          setSkills={setSkills}
+          education={education}
+          setEducation={setEducation} 
+          experience={experience} 
+          setExperience={setExperience}
+        />
+
+        <Preview 
+          personalInfo={personalInfo} 
+          professionalSummary={professionalSummary} 
+          skills={skills} 
+          education={education} 
+          experience={experience} 
+        />
       </div>
     </div>
   )
