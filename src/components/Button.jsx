8c835ef5e70e213870d8faa69b6c1dfb75bcdd38
Sc0rpX <mahfuzurrahman5609@gmail.com>
2026-07-18 07:09:22 +0600

    Refactor Button component to separate padding styles and add ghost variant

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,15 @@
 function Button({ text, icon, variant }) {
-    const baseStyle = "flex justify-center items-center gap-2 font-medium text-sm rounded-sm px-6 py-2 cursor-pointer";
+    const baseStyle = "flex justify-center items-center gap-2 font-medium text-sm rounded-sm cursor-pointer";
+    const paddingStyle = text ? "px-6 py-2" : "p-2";
     const colorVariants = {
         primary: "text-white bg-blue-800 hover:bg-blue-900  transition-colors",
         secondary: "bg-white text-gray-800 border border-gray-500",
-        danger: "bg-white border border-red-700 text-red-700"
+        danger: "bg-white border border-red-700 text-red-700",
+        ghost: "bg-white text-gray-600 hover:text-gray-800 hover:bg-gray-100"
     }
 
     return (
-        <button className={`${baseStyle} ${colorVariants[variant]}`}>
+        <button className={`${baseStyle} ${colorVariants[variant]} ${paddingStyle}`}>
             {icon}
             {text}
         </button>
